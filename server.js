97bbded162bb3f5579cfeb92244704f988e98d33
harsh08881 <silentkiller9369@gmail.com>
2024-12-21 19:52:57 +0530

    add dotenv

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require("express");
 const axios = require("axios");
 const cors = require("cors");
@@ -31,5 +32,5 @@ app.get("/api/option-chain", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
