0aca2dd6cca0644859be81e1ebc6f4427e4f68c9
harsh08881 <silentkiller9369@gmail.com>
2024-12-25 14:31:32 +0530

    add

--- server.js
+++ server.js
@@ -37,5 +37,5 @@ app.get("/api/option-chain", async (req, res) => {
 
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
